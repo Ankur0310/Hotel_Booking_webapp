b581474fa401ce885ef08173f497fd8bda9efd53
Ankur0310 <ankurknp0310@gmail.com>
2023-06-20 13:03:09 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,45 @@
+ require("dotenv").config();
+const express=require('express');
+const connection= require("./db");
+const app=express();
+const authRoute=require("./routes/auth.js");
+const hotelsRoute=require("./routes/hotels");
+const userRoute=require("./routes/user");
+const roomsRoute=require("./routes/rooms");
+const cookieParser=require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+
+connection()
+
+const clientPath=path.join(__dirname, 'client/dist');
+// const adminPath=path.join(__dirname, 'admin/build');
+
+app.use(express.static(clientPath));
+// app.use('/admin', express.static(adminPath));
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(cors());
+
+
+app.use("/auth",authRoute)
+app.use("/hotels",hotelsRoute)
+app.use("/users",userRoute)
+app.use("/rooms",roomsRoute)
+
+
+
+app.use((err,req,res,next)=>{
+    const errorStatus=err.status||500
+    const errorMessage=err.message||"something went wrong"
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack:err.stack,
+    })
+})
+
+const port=process.env.PORT||8800;
+app.listen(port,console.log(`listening on port ${port}...`))
